fb5c03e8ddff15bf4f386cc503af9b615bbf7c2a
Joydeep Dasgupta <81161780+Joydeep-dasgupta-2003@users.noreply.github.com>
2024-07-16 10:48:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '2984ca6d09msh25c46824ff8b97ap1891b6jsnee2e190fa258',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+		'x-rapidapi-key': 'f3080a8eaamsh373d66c50c9efccp154d4fjsnd5d317f5929a',
+		'x-rapidapi-host': 'yahoo-weather5.p.rapidapi.com'
 	}
 };
 const getWeather = (city) => {
-	cityName.innerHTML = city
-	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city, options)
+	cityName.innerHTML = city;
+	fetch('https://yahoo-weather5.p.rapidapi.com/weather?location=' + city +'&u=c', options)
 		.then(response => response.json())
 		.then((response) => {
 			console.log(response);
-			temp.innerHTML = response.temp;
-			feels_like.innerHTML = response.feels_like;
-			humidity.innerHTML = response.humidity;
-			min_temp.innerHTML = response.min_temp;
-			max_temp.innerHTML = response.max_temp;
-			wind_speed.innerHTML = response.wind_speed;
-			wind_degrees.innerHTML = response.wind_degrees;
-			sunrise.innerHTML = response.sunrise;
-			sunset.innerHTML = response.sunset;
+			const weatherData = response.current_observation;
+
+			temp.innerHTML = weatherData.condition.temperature;
+			feels_like.innerHTML = weatherData.wind.chill;
+			humidity.innerHTML = weatherData.atmosphere.humidity;
+			min_temp.innerHTML = response.forecasts[0].low;
+			max_temp.innerHTML = response.forecasts[0].high;
+			wind_speed.innerHTML = weatherData.wind.speed;
+			wind_degrees.innerHTML = weatherData.wind.direction;
+			pressure.innerHTML = weatherData.atmosphere.pressure;
+			visibility.innerHTML = weatherData.atmosphere.visibility;
 		})
 		.catch(err => console.error(err));
 }
+
 submit.addEventListener("click", (e) => {
-	e.preventDefault()
-	getWeather(city.value)
-})
-getWeather("Kolkata")
+	e.preventDefault();
+	getWeather(city.value);
+});
+
+getWeather("Kolkata");
