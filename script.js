2daba7ccda7bd19be68ccde39fdda7d8d4d66089
Joydeep Dasgupta <81161780+Joydeep-dasgupta-2003@users.noreply.github.com>
2024-08-13 21:21:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ const options = {
 	method: 'GET',
 	headers: {
 		'x-rapidapi-key': 'f3080a8eaamsh373d66c50c9efccp154d4fjsnd5d317f5929a',
-		'x-rapidapi-host': 'yahoo-weather5.p.rapidapi.com'
+		'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
 	}
 };
+
 const getWeather = (city) => {
 	cityName.innerHTML = city;
-	fetch('https://yahoo-weather5.p.rapidapi.com/weather?location=' + city +'&u=c', options)
+	
+	fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${city}`, options)
 		.then(response => response.json())
 		.then((response) => {
 			console.log(response);
-			const weatherData = response.current_observation;
-
-			temp.innerHTML = weatherData.condition.temperature;
-			feels_like.innerHTML = weatherData.wind.chill;
-			humidity.innerHTML = weatherData.atmosphere.humidity;
-			min_temp.innerHTML = response.forecasts[0].low;
-			max_temp.innerHTML = response.forecasts[0].high;
-			wind_speed.innerHTML = weatherData.wind.speed;
-			wind_degrees.innerHTML = weatherData.wind.direction;
-			pressure.innerHTML = weatherData.atmosphere.pressure;
-			visibility.innerHTML = weatherData.atmosphere.visibility;
+			const weatherData = response.current;
+			
+			temp.innerHTML = weatherData.temp_c;
+			feels_like.innerHTML = weatherData.feelslike_c;
+			humidity.innerHTML = weatherData.humidity;
+			wind_speed.innerHTML = weatherData.wind_kph;
+			wind_degrees.innerHTML = weatherData.wind_degree;
+			pressure.innerHTML = weatherData.pressure_mb;
+			visibility.innerHTML = weatherData.vis_km;
 		})
 		.catch(err => console.error(err));
 }
